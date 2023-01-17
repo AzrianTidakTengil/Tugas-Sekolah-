b2d1d0fb66b699c918a032414f77729283bfbbd1
Zrriii <71440999+Azrian-web@users.noreply.github.com>
2023-01-18 06:13:48 +0700

    Create Python_Tugas4.py

---
+++ Python_Tugas4.py
@@ -0,0 +1,12 @@
+n = int(input("masukkan nilai = "))
+if n <= 100:
+    if n >= 80:
+        print("A")
+    elif n >= 70:
+        print("B")
+    elif n >= 60:
+        print("C")
+    elif n >= 50:
+        print("D")
+    else:
+        print("E")
