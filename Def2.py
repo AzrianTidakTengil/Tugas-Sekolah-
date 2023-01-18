83125d5e00f8f7413e586619f72d04d8311e86c0
Zrriii <71440999+AzrianTidakTengil@users.noreply.github.com>
2023-01-18 12:09:47 +0700

    Create Def2.py

---
+++ Def2.py
@@ -0,0 +1,13 @@
+def luas(s):
+    return s * s
+
+
+
+oke = luas(20)
+ytta = oke * 4
+
+
+
+print("luas persegi =", luas(5))
+print("namun dapat juga menghasilkan =", oke)
+print("bisa menghasilkan =", ytta+oke)
